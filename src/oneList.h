8523d1a1018a9a92a710986a49f596dbd1f5b10a
neu-rah <ruihfazevedo@gmail.com>
2026-05-19 13:46:01 +0000

    fix namespace collision

--- src/oneList.h
+++ src/oneList.h
@@ -11,7 +11,7 @@
 #include <type_traits>
 #include <utility>
 
-namespace hapi::list {
+namespace hapi::one_list {
 
   // ====================== TypeList ======================
   template<typename... Ts>
